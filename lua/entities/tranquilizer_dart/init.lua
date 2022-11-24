a4cce750310219f9d9f40c38dfc1e46b979dd123
MrMarant <laughymclaugher@gmail.com>
2022-11-24 02:20:20 +0100

    [FIX] blurry vision effect

--- lua/entities/tranquilizer_dart/init.lua
+++ lua/entities/tranquilizer_dart/init.lua
@@ -2,13 +2,11 @@ AddCSLuaFile("shared.lua")
 
 include("shared.lua")
 
-local BlurryVisionTired = "BlurryVisionTired"
+local BlurryVision = "BlurryVision"
 local DelayBlurryVision = "DelayBlurryVision"
-local ResetBlurryVisionTired = "ResetBlurryVisionTired"
 
-util.AddNetworkString( BlurryVisionTired )
+util.AddNetworkString( BlurryVision )
 util.AddNetworkString( DelayBlurryVision )
-util.AddNetworkString( ResetBlurryVisionTired )
 
 -- Set a player to sleep, index start to 1
 local function SetSleepPlayer(victim, timeToGetTired, timeToSleep, step, tableText, index)
@@ -20,7 +18,7 @@ local function SetSleepPlayer(victim, timeToGetTired, timeToSleep, step, tableTe
 	end
 	timer.Create("getting_tired_"..victim:SteamID(), timeToGetTired/step, 1, function()
 		if (!victim.IsTired or !IsValid(victim)) then return end
-		net.Start(BlurryVisionTired)
+		net.Start(BlurryVision)
 		net.WriteTable({step, index})
 		net.Send(victim)
 		factorSpeed = (step - index) / step
@@ -97,6 +95,7 @@ function ENT:Touch(ent)
 		self.IsTouch = true
 		-- Check if the player is not already tired or SCP 035.
 		if (
+		ent:IsPlayer() and
 		-- !table.HasValue(self.jobNotAffected, team.GetName( ent:Team() )) and 
 		!ent.IsTired and 
 		!ent.MaskControl) then
@@ -119,83 +118,3 @@ function ENT:Touch(ent)
 		end
 	end
 end
-
-if CLIENT then
-    net.Receive(BlurryVisionTired, function ( )
-        ply = LocalPlayer()
-        tableData = net.ReadTable()
-        ply.Step = tableData[1]
-        ply.Index = tableData[2]
-        if (tableData[1] == tableData[2]) then
-            ply.BlurryShockTired = false
-            ply.Step = nil
-            ply.Index = nil
-        else
-            ply.BlurryShockTired = true
-        end
-    end)
-
-    net.Receive(DelayBlurryVision, function ( )
-        ply = LocalPlayer()
-        delay = net.ReadFloat()
-        timer.Simple(delay, function()
-            if !IsValid(ply) then return end
-            ply.BlurryShockTired = false
-        end)
-    end)
-
-    net.Receive(ResetBlurryVisionTired, function ( )
-        Check = net.ReadBool()
-        local ply = LocalPlayer()
-        if (Check and ply.BlurryShockTired) then
-            ply.BlurryShockTired = nil
-        end
-    end)
-    
-    function ShockEffectTired()
-        local ply = LocalPlayer()
-        local curTime = FrameTime()
-        if !ply.AddAlpha then ply.AddAlpha = 1 end
-        if !ply.DrawAlpha then ply.DrawAlpha = 0 end
-        if !ply.Delay then ply.Delay = 0 end
-            
-        if ply.BlurryShockTired then 
-            ply.AddAlpha = 0.2 * ply.Index / ply.Step
-            ply.DrawAlpha = 0.99 * ply.Index / ply.Step
-            ply.Delay = 0.05 * ply.Index / ply.Step
-        else
-            ply.AddAlpha = math.Clamp(ply.AddAlpha + curTime * 0.4, 0.2, 1)
-            ply.DrawAlpha = math.Clamp(ply.DrawAlpha - curTime * 0.4, 0, 0.99)
-            ply.Delay = math.Clamp(ply.Delay - curTime * 0.4, 0, 0.05)
-        end
-        
-        DrawMotionBlur( ply.AddAlpha, ply.DrawAlpha, ply.Delay )
-    end
-
-    hook.Add("RenderScreenspaceEffects","ShockEffectTired",ShockEffectTired)
-end
-
--- Function called to remove an blurry vision on the client side.
-function SendResetBlurryVisionTired(victim)
-	net.Start(ResetBlurryVisionTired)
-	net.WriteBool(true)
-	net.Send(victim)
-end
-
--- Function called to remove all effect on death or changed team
-function RemoveEffectGettingTired(victim)
-    if timer.Exists("getting_tired_"..victim:SteamID()) then
-        timer.Remove("getting_tired_"..victim:SteamID())
-    end
-    if timer.Exists("getting_sleepy_"..victim:SteamID()) then
-        timer.Remove("getting_sleepy_"..victim:SteamID())
-	end
-    victim.IsTired = false
-    SendResetBlurryVisionTired(victim)
-end
-
-hook.Add( "PlayerDeath", "PlayerDeath.RemoveTiredEffectGunTranquilizer", RemoveEffectGettingTired )
-hook.Add( "PlayerChangedTeam", "PlayerChangedTeam.RemoveTiredEffectGunTranquilizer", RemoveEffectGettingTired )
-hook.Add("canSleep", "canSleep.tranquilizer_dart.can_wake_up", function (ply)
-	if (ply.Sleeping and (ply.TranquilizedByDart or ply.IsTired)) then return false, "Tu es sous l'effet d'une drogue." end
-end)
--- lua/entities/tranquilizer_dart/shared.lua
+++ lua/entities/tranquilizer_dart/shared.lua
@@ -6,3 +6,89 @@ ENT.Spawnable = true
 ENT.AdminSpawnable = false
 ENT.Category = "BIBI entities"
 ENT.ID = "tranquilizer-dart"
+
+if (SERVER) then
+    util.AddNetworkString( "BlurryVision" )
+    util.AddNetworkString( "DelayBlurryVision" )
+    util.AddNetworkString( "ResetBlurryVision" )
+end
+
+if CLIENT then
+    net.Receive("BlurryVision", function ( )
+        ply = LocalPlayer()
+        tableData = net.ReadTable()
+        ply.Step = tableData[1]
+        ply.Index = tableData[2]
+        if (tableData[1] == tableData[2]) then
+            ply.BlurryShockTired = false
+            ply.Step = nil
+            ply.Index = nil
+        else
+            ply.BlurryShockTired = true
+        end
+    end)
+
+    net.Receive("DelayBlurryVision", function ( )
+        ply = LocalPlayer()
+        delay = net.ReadFloat()
+        timer.Simple(delay, function()
+            if !IsValid(ply) then return end
+            ply.BlurryShockTired = false
+        end)
+    end)
+
+    net.Receive("ResetBlurryVision", function ( )
+        Check = net.ReadBool()
+        local ply = LocalPlayer()
+        if (Check and ply.BlurryShockTired) then
+            ply.BlurryShockTired = nil
+        end
+    end)
+    
+    function ShockEffectTired()
+        local ply = LocalPlayer()
+        local curTime = FrameTime()
+        if !ply.AddAlpha then ply.AddAlpha = 1 end
+        if !ply.DrawAlpha then ply.DrawAlpha = 0 end
+        if !ply.Delay then ply.Delay = 0 end
+            
+        if ply.BlurryShockTired then 
+            ply.AddAlpha = 0.2 * ply.Index / ply.Step
+            ply.DrawAlpha = 0.99 * ply.Index / ply.Step
+            ply.Delay = 0.05 * ply.Index / ply.Step
+        else
+            ply.AddAlpha = math.Clamp(ply.AddAlpha + curTime * 0.4, 0.2, 1)
+            ply.DrawAlpha = math.Clamp(ply.DrawAlpha - curTime * 0.4, 0, 0.99)
+            ply.Delay = math.Clamp(ply.Delay - curTime * 0.4, 0, 0.05)
+        end
+        
+        DrawMotionBlur( ply.AddAlpha, ply.DrawAlpha, ply.Delay )
+    end
+
+    hook.Add("RenderScreenspaceEffects","RenderScreenspaceEffects.ShockEffectTired",ShockEffectTired)
+end
+
+-- Function called to remove an blurry vision on the client side.
+function SendResetBlurryVision(victim)
+	net.Start("ResetBlurryVision")
+	net.WriteBool(true)
+	net.Send(victim)
+end
+
+-- Function called to remove all effect on death or changed team
+function RemoveEffectGettingTired(victim)
+    if timer.Exists("getting_tired_"..victim:SteamID()) then
+        timer.Remove("getting_tired_"..victim:SteamID())
+    end
+    if timer.Exists("getting_sleepy_"..victim:SteamID()) then
+        timer.Remove("getting_sleepy_"..victim:SteamID())
+	end
+    victim.IsTired = false
+    SendResetBlurryVision(victim)
+end
+
+hook.Add( "PlayerDeath", "PlayerDeath.RemoveTiredEffectGunTranquilizer", RemoveEffectGettingTired )
+hook.Add( "PlayerChangedTeam", "PlayerChangedTeam.RemoveTiredEffectGunTranquilizer", RemoveEffectGettingTired )
+hook.Add("canSleep", "canSleep.tranquilizer_dart.can_wake_up", function (ply)
+	if (ply.Sleeping and (ply.TranquilizedByDart or ply.IsTired)) then return false, "Tu es sous l'effet d'une drogue." end
+end)
